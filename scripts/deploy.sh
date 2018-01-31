4249a4acf39d5013effdbb2fd37f54fbc1ab7e31
Andy <andy.rash1996@gmail.com>
2018-01-31 16:08:49 -0600

    removed extranous portion of deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,20 +8,12 @@ debug() {
     git config --global push.default matching
     git remote add deploy ssh://git@$DEV_REMOTE_HOST:$DEV_REMOTE_PORT$DEV_REMOTE_DIR
     git push deploy dev
-    
-    ssh andy@$DEV_REMOTE_HOST -p $DEV_REMOTE_PORT <<EOF
-        cd $DEV_REMOTE_DIR
-EOF
 }
 
 release() {
     git config --global push.default matching
     git remote add deploy ssh://git@$PROD_REMOTE_HOST:$PROD_REMOTE_PORT$PROD_REMOTE_DIR
     git push deploy dev
-
-    ssh andy@$PROD_REMOTE_HOST -p $PROD_REMOTE_PORT <<EOF
-        cd $PROD_REMOTE_DIR
-EOF
 }
 
 case $1 in
