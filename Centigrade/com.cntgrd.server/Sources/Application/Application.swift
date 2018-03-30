6d596a92cbf78b291b0b9dc80ebbd7b821247a95
Andy <andy.rash1996@gmail.com>
2018-03-30 11:14:03 -0500

    organized data routes into an initializeDataRoutes function

--- Centigrade/com.cntgrd.server/Sources/Application/Application.swift
+++ Centigrade/com.cntgrd.server/Sources/Application/Application.swift
@@ -12,60 +12,13 @@ public let health = Health()
 
 public class App {
     let router = Router()
+    let requestHelper = RequestHelper()
 
     public init() throws {
-        router.get("/hello") { _, response, next in
-            response.send(json: ["message": "hello world"])
-            next()
-        }
-
-        let tempPressureHumidityUUID = UUID().uuidString
-        let atmosphereUUID = UUID().uuidString
-        let stationUUID = UUID().uuidString
-        
-        let requestHelper = RequestHelper()
-        
-        router.get("/data") { request, response, next in
-            let acceptHeader = requestHelper.acceptHeader(headers: request.headers)
-            response.headers.append("Content-Type", value: acceptHeader.contentType)
-            
-            
-            let temperature = Centigrade_Measurement(time: UInt64(NSDate().timeIntervalSince1970),
-                                                     measurement: .temperature(Centigrade_Celsius(value: 22)),
-                                                     sensor: Centigrade_Sensor(uuid: tempPressureHumidityUUID, sensorType: .temperature))
-            let humidity = Centigrade_Measurement(time: UInt64(NSDate().timeIntervalSince1970),
-                                                  measurement: .humidity(Centigrade_Percent(value: 41)),
-                                                  sensor: Centigrade_Sensor(uuid: tempPressureHumidityUUID, sensorType: .humidity))
-            let pressure = Centigrade_Measurement(time: UInt64(NSDate().timeIntervalSince1970),
-                                                  measurement: .pressure(Centigrade_Hectopascals(value: 1013)),
-                                                  sensor: Centigrade_Sensor(uuid: tempPressureHumidityUUID, sensorType: .pressure))
-            
-            let totalVOC = Centigrade_Measurement(time: UInt64(NSDate().timeIntervalSince1970),
-                                                  measurement: .totalVocs(Centigrade_PartsPerBillion(value: 7)),
-                                                  sensor: Centigrade_Sensor(uuid: atmosphereUUID, sensorType: .totalVoc))
-            let equivalentC02 = Centigrade_Measurement(time: UInt64(NSDate().timeIntervalSince1970),
-                                                       measurement: .equivalentCo2(Centigrade_PartsPerMillion(value: 410)),
-                                                       sensor: Centigrade_Sensor(uuid: atmosphereUUID, sensorType: .equivalentCo2))
-            
-            let measurementList = [temperature, humidity, pressure, totalVOC, equivalentC02]
-            let measurements = Centigrade_StationRecentMeasurements(uuid: stationUUID, measurements: measurementList)
-            
-            switch acceptHeader {
-            case .json:
-                let data = try measurements.jsonUTF8Data()
-                response.send(data: data)
-            case .protobuf:
-                let data = try measurements.serializedData()
-                response.send(data: data)
-            }
-            
-            next()
-        }
-        
+        initializeDataRoutes(router: router, requestHelper: requestHelper)
     }
 
     func postInit() throws {
-        // Endpoints
         initializeHealthRoutes(app: self)
     }
 
